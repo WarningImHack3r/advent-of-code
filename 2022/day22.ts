27e43d39d5f13c7911888d42b06c6c7c0b66faca
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2022-12-12 22:57:44 +0100

    Remove not started challenges

--- 2022/day22.ts
+++
@@ -1,9 +0,0 @@
-import { readFile } from "fs";
-
-readFile("2022/inputs/input22.txt", (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    const input = data.toString().split("\n").filter(x => x.length > 0);
-});
