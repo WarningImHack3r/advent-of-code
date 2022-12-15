bc3b603f8a611e554f22592bf2f2358bf8d53130
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2022-12-16 00:14:59 +0100

    Finally manage to finish 7

---
+++ 2022/README.md
@@ -0,0 +1,11 @@
+# 2022
+
+Unwritten rules for this year
+
+## Day 7
+- `/aaa/bbb/aaa` path is **possible**. Managing depth correctly is important.
+- `/eee/ccc/aaa` path is **_also_ possible**. This means you have to handle both depth and parent folder!
+
+> Is a combination of 2 repeated folders with the same depth possible? Like both `/aaa/bbb/ccc/aaa` and `/aaa/ddd/ccc/aaa`?
+
+I have no clue
--- 2022/day7.ts
+++ 2022/day7.ts
@@ -7,9 +7,7 @@ readFile(__dirname + "/inputs/input7.txt", (err, data) => {
     }
     const input = data.toString().split("\n").filter(x => x.length > 0);
 
-    let parent = "";
-    let location = "";
-    let depth = 0;
+    let pwd: string[] = [];
     const dirsChilds: Record<string, string[]> = {};
     const dirsSizes: Record<string, number> = {};
     // Loop through each line of input
@@ -22,51 +20,43 @@ readFile(__dirname + "/inputs/input7.txt", (err, data) => {
                 // Change directory
                 const destDir = command.split(" ")[1];
                 if (destDir === "..") {
-                    depth--;
-                    console.log("[$] Parent of", location, "is", parent);
-                    location = parent;
+                    pwd.pop();
+                } else if (destDir === "/") {
+                    pwd = [];
                 } else {
-                    depth = destDir === "/" ? 0 : depth + 1;
-
-                    let prefix = depth > 0 ? depth.toString() : "";
-                    const parents = Object.keys(dirsChilds).filter(x => dirsChilds[x].includes(location));
-                    if (parents.length > 1 && !dirsChilds[parent].includes(location)) {
-                        prefix += "-" + (parents.indexOf(parent) + 1);
-                        console.log("[$] Found multiple parents", parents, "for location", location, "-> new prefix", prefix);
-                    }
-                    prefix += "_";
-
-                    if (destDir !== "/") {
-                        parent = location;
-                    }
-                    location = prefix + destDir;
+                    // Real directory
+                    pwd.push(destDir);
                 }
+                const location = "/" + pwd.join("/");
                 dirsChilds[location] = dirsChilds[location] || [];
                 dirsSizes[location] = dirsSizes[location] || 0;
-                console.log("[$] Changed directory to", location, "with childs", dirsChilds[location], "and size", dirsSizes[location]);
+                console.log("[$] Changed directory to", destDir, "with childs", dirsChilds[location], "and size", dirsSizes[location]);
             }
+            // The only other command is "ls", which is not needed
         } else {
-            // Output of a ls command
+            // Output of a ls command (only possible command output)
+            const location = "/" + pwd.join("/");
             if (line.startsWith("dir ")) {
-                dirsChilds[location].push((depth + 1).toString() + "_" + line.substring(4));
-                console.log("[->] Added child", (depth + 1).toString() + "_" + line.substring(4), "to", location);
+                dirsChilds[location].push(line.substring(4));
+                console.log("[->] Added child", line.substring(4), "to", location);
             } else {
                 dirsSizes[location] += parseInt(line.substring(0, line.indexOf(" ")));
                 console.log("[->] Added size", line.substring(0, line.indexOf(" ")), "to", location);
             }
         }
     });
-    Object.keys(dirsChilds).forEach(dir => {
-        // console.log("[+] Starting for", dir, "with size", dirsSizes[dir]);
-        const size = sizeOfSubdirectories(dir, dirsChilds, dirsSizes);
-        dirsSizes[dir] += size;
-        // console.log("[*] New size for", dir, "with size", dirsSizes[dir]);
-    });
+    // Append size of childs to size of parent recursively
+    Object.keys(dirsChilds).forEach(dir => dirsSizes[dir] += sizeOfSubdirectories(dir, dirsChilds, dirsSizes));
 
-    console.log("[o] Result:", Object.values(dirsSizes).filter(x => x <= 100_000).reduce((a, b) => a + b));
+    // Display result
+    console.log("[o] Total size of dirs <= 100_000:", Object.values(dirsSizes).filter(x => x <= 100_000).reduce((a, b) => a + b)); // part 1
+    const freeSpace = 70_000_000 - dirsSizes["/"];
+    const minDeleteNeeded = 30_000_000 - freeSpace;
+    console.log("[o] Biggest dir to delete to free space:", Object.values(dirsSizes).sort((a, b) => a - b).filter(x => x >= minDeleteNeeded)[0]); // part 2
 });
 
-function sizeOfSubdirectories(directory: string, dirsChilds: Record<string, string[]>, dirsSizes: Record<string, number>): number {
-    // console.log("[o] Calculating size of", directory, `(${dirsSizes[directory]}) with childs`, dirsChilds[directory]);
-    return dirsChilds[directory].map(child => dirsSizes[child] + sizeOfSubdirectories(child, dirsChilds, dirsSizes)).reduce((a, b) => a + b, 0);
+function sizeOfSubdirectories(directory: string, dirsChilds: Record<string, string[]>, dirsSizes: Record<string, number>, recursive = false): number {
+    console.log("[c] Calculating size of", directory, `(${dirsSizes[directory]}) with childs`, dirsChilds[directory], recursive ? "(recursive)" : "");
+    const path = directory + (directory === "/" ? "" : "/");
+    return dirsChilds[directory].map(child => dirsSizes[path + child] + sizeOfSubdirectories(path + child, dirsChilds, dirsSizes, true)).reduce((a, b) => a + b, 0);
 }
