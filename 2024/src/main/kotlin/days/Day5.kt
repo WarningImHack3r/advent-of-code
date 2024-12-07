4ba9e6e109d2d134109c4d77983828bc3476ee78
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2024-12-07 15:33:58 +0100

    2024 day 5

---
+++ 2024/src/main/kotlin/days/Day5.kt
@@ -0,0 +1,61 @@
+package days
+
+@Suppress("unused")
+object Day5 : DayBase(5) {
+    val orderingMap = mutableMapOf<Int, List<Int>>()
+    val sequences = mutableListOf<List<Int>>()
+
+    override fun solve(input: List<String>) {
+        // Parsing
+        var isParsingSequences = false
+        for (line in input) {
+            if (line.isEmpty()) {
+                isParsingSequences = true
+                continue
+            }
+
+            if (isParsingSequences) {
+                sequences.add(line.split(",").map(String::toInt))
+            } else {
+                val (base, beforePage) = line.split("|").map(String::toInt)
+                orderingMap[base] = orderingMap[base]?.let {
+                    it + listOf(beforePage)
+                } ?: listOf(beforePage)
+            }
+        }
+
+        val incorrectSequences = mutableListOf<List<Int>>()
+
+        var sum = 0
+        sequences@ for (line in sequences) {
+            for ((i, digit) in line.withIndex()) {
+                val afterPages = orderingMap[digit] ?: emptyList()
+                if (afterPages.isEmpty()) continue
+                for (after in line.subList(0, i)) {
+                    if (after in afterPages) {
+                        // ChatGPT helped me to translate my thoughts into this `sortedWith`,
+                        // sorry not sorry lol
+                        incorrectSequences.add(line.sortedWith { a, b ->
+                            when {
+                                orderingMap[a]?.contains(b) == true -> -1
+                                orderingMap[b]?.contains(a) == true -> 1
+                                else -> 0
+                            }
+                        })
+                        continue@sequences
+                    }
+                }
+            }
+            sum += line[line.size / 2]
+        }
+
+        setPart1Answer(sum)
+
+        sum = 0
+        for (line in incorrectSequences) {
+            sum += line[line.size / 2]
+        }
+
+        setPart2Answer(sum)
+    }
+}
