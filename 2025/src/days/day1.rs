0caa00ff71c7470488abc5cb98cb2d941a69cd28
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2025-12-06 00:10:26 +0100

    feat(2025): init Rust, day 1

---
+++ 2025/src/days/day1.rs
@@ -0,0 +1,32 @@
+pub fn answers(input: &str) -> (i32, i32) {
+    let moves = input.trim().lines().map(|line| {
+        let letter = line.chars().nth(0).unwrap();
+        let count = line.get(1..).unwrap().parse::<i32>().unwrap();
+        match letter {
+            'L' => -count,
+            _ => count,
+        }
+    });
+
+    let mut zeroes = 0;
+    let mut wraps = 0;
+    let mut index = 50;
+    for mov in moves {
+        let diff = index + mov;
+        wraps += match mov {
+            // stole ONLY THIS MATCH on GH because I was going crazy sorry...
+            // https://github.com/yi-json/aoc-2025/blob/main/src/bin/day01.rs#L71
+            // I was VERY CLOSE with `diff.div_euclid(100).abs()`
+            // did I mention I suck at math?
+            mov if mov < 0 => (index - 1).div_euclid(100) - (diff - 1).div_euclid(100),
+            mov if mov > 0 => diff.div_euclid(100) - index.div_euclid(100),
+            _ => 0,
+        };
+        index = diff.rem_euclid(100);
+        if index == 0 {
+            zeroes += 1;
+        }
+    }
+
+    (zeroes, wraps)
+}
---
+++ 2025/src/main.rs
@@ -0,0 +1,19 @@
+use std::{env, fs};
+
+use aoc2025::days;
+
+fn read_file(day: u8) -> String {
+    let inputs_file = env::current_dir()
+        .unwrap()
+        .join("inputs")
+        .join(format!("input{day}.txt"));
+    fs::read_to_string(inputs_file).expect("could not open input file")
+}
+
+fn main() {
+    const DAY: u8 = 1;
+    let file = read_file(DAY);
+    let (part1, part2) = days::day1::answers(&file);
+    println!("Day {DAY} part 1: {part1}");
+    println!("Day {DAY} part 2: {part2}");
+}
